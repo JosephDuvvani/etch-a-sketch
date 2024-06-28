fc1ec7e5f47f5b6c77e821c1e129048a60cf0eac
JosephDuvvani <josephduvvani@gmail.com>
2024-06-28 21:54:05 +0200

    Change each square's color when mouse hovers over it

--- myScript.js
+++ myScript.js
@@ -7,10 +7,19 @@ for (let i = 1; i <= 16; i++) {
     row.classList.add(`row${i}`);
     row.classList.add('row');
     for (let j = 1; j <= 16; j++) {
-        let column = document.createElement('div');
-        column.classList.add(`column${j}`);
-        column.classList.add('square');
-        row.appendChild(column);
+        let square = document.createElement('div');
+        square.classList.add(`column${j}`);
+        square.classList.add('square');
+        square.id = `square${i}${j}`;
+        row.appendChild(square);
     }
     container.appendChild(row);
+}
+
+const squareList = document.querySelectorAll('.square');
+
+for (let i = 0; i < squareList.length; i++) {
+    squareList[i].addEventListener('mouseenter', () => {
+        squareList[i].style.backgroundColor = 'red';
+    });
 }
